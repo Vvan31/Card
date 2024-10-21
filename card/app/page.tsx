6d5fb5e69f81c3d88df6388908cf2325406b1962
vivianosorio <71792220+Vvan31@users.noreply.github.com>
2024-10-20 21:43:35 -0700

    Created new branch
    Bring changes to create a n authenticated project panel display.

--- card/app/page.tsx
+++ card/app/page.tsx
@@ -2,19 +2,18 @@ import Image from "next/image";
 import VirtualCard from "./components/VirtualCard";
 import ScrollUpButton from "./components/ScrollUpButton";
 import ScrollSubtitle from "./components/ScrollSubtitle";
+import {ProjectsDisplay} from "./components/ProjectsDisplay";
 
 export default function Home() {
   return (
-    <>
-      <div className="flex justify-center items-center my-8">
-        {/* <h2 className="text-4xl font-bold text-gray-800 bg-clip-text text-left ml-20">
-          Carta a Papá
-        </h2> */}
-        <ScrollSubtitle />
+    <div className="bg-white h-screen w-screen">
+      <div className="flex justify-center items-center my-8 flex-col mx-8 ">
+        <h2 className="text-xl md:text-4xl font-bold text-gray-800 bg-clip-text mb-3 md:mb-8 l">
+          Welcome
+        </h2>
+          <ProjectsDisplay   />
       </div>
-      <VirtualCard />
-      <ScrollUpButton />
-    </>
+    </div>
   );
 }
 Home.displayName = 'Home';
